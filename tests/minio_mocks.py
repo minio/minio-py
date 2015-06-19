ca59f3a29f9fdb043c083f077f2809b36e6d8275
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2015-06-19 12:14:20 -0700

    Moving mocks to their own file to not clutter tests

---
+++ tests/minio_mocks.py
@@ -0,0 +1,42 @@
+# Minimal Object Storage Library, (C) 2015 Minio, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from nose.tools import eq_
+
+__author__ = 'minio'
+
+
+class MockResponse(object):
+    def __init__(self, method, url, headers, status):
+        self.method = method
+        self.url = url
+        self.headers = headers
+        self.status = status
+
+    def mock_verify(self, method, url, headers):
+        eq_(self.method, method)
+        eq_(self.url, url)
+        eq_(self.headers, headers)
+
+
+class MockConnection(object):
+    def __init__(self):
+        self.requests = []
+
+    def mock_add_request(self, request):
+        self.requests.append(request)
+
+    def request(self, method, url, headers):
+        return_request = self.requests.pop(0)
+        return_request.mock_verify(method, url, headers)
+        return return_request
