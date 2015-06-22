3ae5dc9a4325251033d3db9cae0d80eb4812815d
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2015-06-21 20:53:48 -0700

    Add lazy iterator

---
+++ minio/lazy_iterator.py
@@ -0,0 +1,33 @@
+# Minimal Object Storage Library, (C) 2015 Minio, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+__author__ = 'minio'
+
+class LazyIterator(object):
+    def __init__(self, populator):
+        self.populator = populator
+        self.values = []
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.populator is None:
+            # should never see this, but we'll be defensive
+            raise StopIteration()
+        if len(self.values) == 0:
+            self.values, self.populator = self.populator()
+        if len(self.values) > 0:
+            return self.values.pop(0)
+        raise StopIteration()
+
