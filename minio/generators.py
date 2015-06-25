20fc611df6e8c1123f79c59ebd5a5150d7ffd21c
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2015-06-24 21:12:30 -0700

    Setting up fetch for list objects, no parsing yet

--- minio/generators.py
+++ minio/generators.py
@@ -11,50 +11,56 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import requests
+from .helpers import get_target_url
+from .parsers import parse_list_objects
 from .signer import sign_v4
 
 __author__ = 'minio'
 
 class ListObjectsIterator:
-    def __init__(self, scheme, location, bucket, prefix, recursive):
-        self.scheme = scheme
-        self.location = location
-        self.bucket = bucket
-        self.prefix = prefix
-        self.recursive = recursive
-        self.results = []
-        self.complete = False
+    def __init__(self, scheme, location, bucket, prefix, recursive, access_key, secret_key):
+        self._scheme = scheme
+        self._location = location
+        self._bucket = bucket
+        self._prefix = prefix
+        self._recursive = recursive
+        self._results = []
+        self._complete = False
+        self._access_key = access_key
+        self._secret_key = secret_key
 
     def __iter__(self):
         return self
 
     def next(self):
-        if self.complete:
+        if self._complete:
             raise StopIteration
-        if len(self.results) == 0:
-            self._fetch()
-        if len(self.results) == 0:
-            self.complete = True
+        if len(self._results) == 0:
+            self._results = self._fetch()
+        if len(self._results) == 0:
+            self._complete = True
             raise StopIteration
-        return self.results.pop(0)
+        return self._results.pop(0)
 
     def _fetch(self):
         query = {}
-        if self.prefix is not None:
-            query['prefix'] = self.prefix
-        if not self.recursive:
+        if self._prefix is not None:
+            query['prefix'] = self._prefix
+        if not self._recursive:
             query['delim'] = '/'
-        url = get_target_url(scheme, location, bucket=bucket,query=query)
+
+        url = get_target_url(self._scheme, self._location, bucket=self._bucket,query=query)
 
         method = 'GET'
         headers = {}
 
-        headers = sign_v4(method=method, url=url, headers=headers, access_key=client._access_key,
-                          secret_key=client._secret_key)
+        headers = sign_v4(method=method, url=url, headers=headers, access_key=self._access_key,
+                          secret_key=self._secret_key)
 
         response = requests.get(url, headers=headers)
 
         if response.status_code != 200:
             self.client.parse_error(response)
-
-        pass
+        return parse_list_objects(response.content)
+        # parse
