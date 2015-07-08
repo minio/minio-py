c576f8a703057008f5d9b4312021e2ad23bd759a
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2015-07-07 19:34:12 -0700

    Revert "Move tests into tests/functional and integration into tests/integration"
    This reverts commit 86f9a329c141a732116eae8163770bdc4c4d0750.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     version=get_version(),
     install_requires=['requests', 'pytz'],
     tests_require=['nose', 'mock'],
-    packages=find_packages(exclude=['tests*', 'integration']),
+    packages=find_packages(exclude=['tests*']),
     scripts=[],
     setup_requires=['nose>=1.0'],
     test_suite='tests',
