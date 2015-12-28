ff7e989676ba35c2941d7fc3964126e784a3d1d9
Harshavardhana <harsha@harshavardhana.net>
2015-12-28 14:56:18 -0800

    fput_object: Complete multipart upload result should be parsed.
    Additionally this patch also removes region from cache upon any error.

--- minio/definitions.py
+++ minio/definitions.py
@@ -74,6 +74,29 @@ class Object(object):
                                     self.is_dir)
 
 
+class MultipartUploadResult(object):
+    """
+    A completed multipart upload metadata
+         :class:`MultipartUploadResult <MultipartUploadResult>`.
+
+    :param bucket_name: Bucket name.
+    :param object_name: Object name.
+    :param location: Object uploaded location.
+    :param etag: Object final etag.
+    """
+    def __init__(self, bucket_name, object_name, location, etag):
+        self.bucket_name = bucket_name
+        self.object_name = object_name
+        self.location = location
+        self.etag = etag
+
+    def __str__(self):
+        string_format = ('<IncompleteUpload: bucket_name: {0}'
+                         ' object_name: {1} location: {2} etag: {3}>')
+        return string_format.format(self.bucket_name, self.object_name,
+                                    self.location, self.etag)
+
+
 class IncompleteUpload(object):
     """
     A partially uploaded object's metadata
