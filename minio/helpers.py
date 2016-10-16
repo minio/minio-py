735ed000337f0b1dbbb24a0dae4e5b6ed5bd3852
Matias Kainulainen <matias.kainulainen@solita.fi>
2016-10-16 12:08:32 +0300

    Add support for cn-north-1 endpoint (#396)

--- minio/helpers.py
+++ minio/helpers.py
@@ -207,6 +207,7 @@ AWS_S3_ENDPOINT_MAP = {
     'ap-southeast-1': 's3-ap-southeast-1.amazonaws.com',
     'ap-northeast-1': 's3-ap-northeast-1.amazonaws.com',
     'ap-northeast-2': 's3-ap-northeast-2.amazonaws.com',
+    'cn-north-1': 's3.cn-north-1.amazonaws.com.cn'
 }
 
 def get_s3_endpoint(region):
