7107c84183cf5fb4deff68c0a16ab9f1c0b4c37e
Minio Trusted <trusted@minio.io>
2019-04-24 22:57:45 +0000

    Update version to next release

--- minio/__init__.py
+++ minio/__init__.py
@@ -29,7 +29,7 @@ minio - MinIO Python Library for Amazon S3 Compatible Cloud Storage
 
 __title__ = 'minio-py'
 __author__ = 'MinIO, Inc.'
-__version__ = '4.0.15'
+__version__ = '4.0.16'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015, 2016, 2017 MinIO, Inc.'
 
