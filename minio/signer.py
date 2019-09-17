2c90282709b9232e16894ce6b1bac895bb9bbcb4
Anis Elleuch <vadmeste@users.noreply.github.com>
2019-09-17 21:30:17 +0100

    Do not encode ~ in V4 S3 signing (#798)

--- minio/signer.py
+++ minio/signer.py
@@ -251,7 +251,9 @@ def generate_canonical_request(method, parsed_url, headers, signed_headers, cont
     :param headers: HTTP header dictionary.
     :param content_sha256: Content sha256 hexdigest string.
     """
-    lines = [method, parsed_url.path, parsed_url.query]
+    # Should not encode ~. Decode it back if present.
+    parsed_url_path = parsed_url.path.replace("%7E", "~")
+    lines = [method, parsed_url_path, parsed_url.query]
 
     # Headers added to canonical request.
     header_lines = []
