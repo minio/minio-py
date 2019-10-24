cd1da0c0be50169c2cce3911a1a0a9362c35f4e5
Felipe <felipe@codecov.io>
2019-10-24 13:00:36 -0700

    Added aws, minio, iam, chain and static credentials

---
+++ minio/credentials/credentials.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# MinIO Python Library for Amazon S3 Compatible Cloud Storage, (C)
+# 2015, 2016, 2017, 2018, 2019 MinIO, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import abc
+from datetime import datetime, timedelta
+
+class Value(object):
+    def __init__(self, access_key=None, secret_key=None, session_token=None):
+        self._access_key = access_key
+        self._secret_key = secret_key
+        self._session_token = session_token
+
+        #TODO No signature type in python. How should signatures be handled
+        self._signer_type = None
+
+class Provider(abc.ABC):
+    @abc.abstractmethod
+    def retrieve(self):
+        pass
+
+    @abc.abstractmethod
+    def is_expired(self):
+        pass
+
+class Expiry(object):
+    def __init__(self):
+        self._expiration = None
+
+    def set_expiration(self, expiration, time_delta=None):
+        self._expiration = expiration
+        if time_delta is not None:
+            self._expiration = self._expiration + time_delta
+
+    def is_expired(self):
+        if self._expiration is None:
+            return True
+        return self._expiration < datetime.now()
+
+class Credentials(object):
+    def __init__(self, forceRefresh=True, provider=None):
+        self._creds = None
+        self._forceRefresh = forceRefresh
+        self._provider = provider
+
+    def get(self):
+        if self.is_expired():
+            try:
+                creds = self._provider.retrieve()
+            except:
+                # TODO: Create new error class to raise here?
+                raise
+            self._creds = creds
+            self._forceRefresh = False
+        return self._creds
+
+    def expire(self):
+        self._forceRefresh = True
+
+    def is_expired(self):
+        return self._forceRefresh or self._provider.is_expired()
+
+
+
---
+++ minio/credentials/file_minio_client.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# MinIO Python Library for Amazon S3 Compatible Cloud Storage, (C)
+# 2015, 2016, 2017, 2018, 2019 MinIO, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import configparser
+import os, json
+import sys
+
+from .credentials import Provider, Value
+
+class FileMinioClient(Provider):
+    def __init__(self, filename=None, alias=None, retrieved=False):
+        super(Provider, self).__init__()
+        self._filename = filename
+        self._alias = alias
+        self._retrieved = retrieved
+
+    def retrieve(self):
+        if self._filename == "" or None:
+            home_dir = os.environ.get('HOME')
+            self._filename = os.path.join(home_dir, '.mc', 'config.json')
+            if sys.platform == 'win32':
+                self._filename = os.path.join(home_dir, 'mc', 'config.json')
+        if self._alias == "" or self._alias is None:
+            self._alias = os.environ.get('MINIO_ALIAS')
+            if self._alias == "" or self._alias is None:
+                self._alias = "s3"
+        
+        self._retrieved = False
+        config = open(self._filename, 'r')
+        doc = json.load(config)
+        creds = doc['hosts'][self._alias]
+        self._retrieved = True
+        # TODO: What to do with signers
+        return Value(
+            access_key=creds['accessKey'],
+            secret_key=creds['secretKey']
+        )
+
+    def is_expired(self):
+        return not self._retrieved
