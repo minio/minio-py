ec2a745979eff30bc6761099d8605db762bfcbc2
Brian Thorne <brian@hardbyte.nz>
2020-04-13 16:13:27 +1200

    Refactor aws_iam to also use a response parser
    Add copyright header to credentials/parsers.py

--- minio/credentials/parsers.py
+++ minio/credentials/parsers.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+# MinIO Python Library for Amazon S3 Compatible Cloud Storage, (C)
+# 2020 MinIO, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from xml.etree import ElementTree
 
 from .credentials import Value
@@ -20,7 +36,7 @@ def parse_iam_credentials(data):
         - a :class:`~minio.credentials.Value` instance with the temporary credentials.
         - A :class:`DateTime` instance of when the credentials expire.
     """
-    expiration = _iso8601_to_utc_datetime(data['Expiration'])
+    expiration = datetime.strptime(data['Expiration'], '%Y-%m-%dT%H:%M:%SZ')
     return Value(
         access_key=data['AccessKeyId'],
         secret_key=data['SecretAccessKey'],
