766b3dd44a680662bc7a3600a06e8b431fb812cf
Brian Thorne <brian@hardbyte.nz>
2020-04-16 10:40:28 +1200

    Add notes to assume role example

--- examples/assume_role.py
+++ examples/assume_role.py
@@ -13,18 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Notes:
-#  - You cannot use root user credentials to call AssumeRole.
-#  - YOUR-ACCESSKEYID and YOUR-SECRETACCESSKEY are
-#    dummy values, please replace them with original values.
+#
+# AssumeRoleProvider will call the Simple Token Service (STS) to retrieve
+# temporary credentials.
+#
+# - You can't call AssumeRole as a root user on either MinIO or AWS.
+#   For MinIO add a non-root user using the minio client `mc`:
+#
+#     mc admin user add myminio YOUR-ACCESSKEYID YOUR-SECRETACCESSKEY
+#   On AWS you will need an IAM user with the sts:AssumeRole action allowed,
+#   and a target role.
+# - The credentials will be valid for between 15 minutes and 12 hours.
+# - An access policy can be applied to the temporary credentials. The
+#   resulting permissions are the intersection of the role's existing policy
+#   and the optionally provided policy. You cannot grant more permissions than
+#   those allowed by the policy of the role that is being assumed.
+# - YOUR-ACCESSKEYID and YOUR-SECRETACCESSKEY are
+#   dummy values, please replace them with original values.
+# - To use minio with AWS, the `Minio` client that is passed to the
+#   AssumeRoleProvider must have the endpoint 'sts.amazonaws.com', and the
+#   RoleARN argument must be provided.
 
 from minio import Minio
 from minio.credentials import AssumeRoleProvider, Credentials
 
 client = Minio('localhost:9000',
                access_key='YOUR-ACCESSKEYID',
-               secret_key='YOUR-SECRETACCESSKEY',
-               region='us-east-1', secure=False)
+               secret_key='YOUR-SECRETACCESSKEY'
+               )
 
 restricted_upload_policy = """{
   "Version": "2012-10-17",
@@ -52,4 +68,4 @@ print(temp_creds.get().access_key)
 print(temp_creds.get().secret_key)
 
 # Initialize Minio client with the temporary credentials
-restricted_client = Minio('localhost:9000', credentials=temp_creds, region='us-east-1', secure=False)
+restricted_client = Minio('localhost:9000', credentials=temp_creds)
