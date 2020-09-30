64431233de8399ace66f71fd60bc7b895ab2c6ab
Bala.FA <bala.gluster@gmail.com>
2020-09-30 19:10:35 +0530

    add {set,get,delete}_{bucket,object}_tags apis

---
+++ examples/set_object_tags.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# MinIO Python Library for Amazon S3 Compatible Cloud Storage.
+# Copyright (C) 2020 MinIO, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Note: YOUR-ACCESSKEYID, YOUR-SECRETACCESSKEY and my-bucketname are
+# dummy values, please replace them with original values.
+
+from minio import Minio
+from minio.commonconfig import Tags
+
+client = Minio(
+    "play.min.io",
+    access_key="Q3AM3UQ867SPQQA43P2F",
+    secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+)
+
+tags = Tags.new_object_tags()
+tags["Project"] = "Project One"
+tags["User"] = "jsmith"
+client.set_object_tags("my-bucketname", "my-objectname", tags)
