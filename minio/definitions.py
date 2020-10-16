568ae9e00a8a8fc5bb3a484aead711700ee1178b
Bala.FA <bala.gluster@gmail.com>
2020-10-16 11:48:11 +0530

    fix Host header in request correctly

--- minio/definitions.py
+++ minio/definitions.py
@@ -212,7 +212,7 @@ class BaseURL:
             (method == "PUT" and not object_name and not query_params) or
 
             # GetBucketLocation API requires path style in Amazon AWS S3.
-            (query_params and query_params.get("location")) or
+            (query_params and "location" in query_params) or
 
             # Use path style for bucket name containing '.' which causes
             # SSL certificate validation error.
