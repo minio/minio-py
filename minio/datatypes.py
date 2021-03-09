f07994e77ad52daac993f22d206bdb26f7ec4376
GreatBahram <aghaee.bahram@gmail.com>
2021-03-09 17:03:35 +0330

    add string representation as well as equality method

--- minio/datatypes.py
+++ minio/datatypes.py
@@ -51,6 +51,19 @@ class Bucket:
         """Get creation date."""
         return self._creation_date
 
+    def __repr__(self):
+        return "{}({!r})".format(type(self).__name__, self.name)
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, Bucket):
+            return self.name == other.name
+        elif isinstance(other, str):
+            return self.name == other
+        return NotImplemented
+
 
 class ListAllMyBucketsResult:
     """LissBuckets API result."""
