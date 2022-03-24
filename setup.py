78702e0b427007148c4ac8c77ee0d9f6083c53b9
pgajdos <pgajdos@suse.cz>
2022-03-24 14:16:39 +0100

    remove mock from setup.py and Makefile

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     package_dir={"minio": "minio"},
     packages=["minio", "minio.credentials"],
     install_requires=["certifi", "urllib3"],
-    tests_require=["mock"],
+    tests_require=[],
     license="Apache License 2.0",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
