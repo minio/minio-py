170461b1c6bb329b980bd8ba25bf57639170f1f4
VerdantForge <123564396+VerdantForge@users.noreply.github.com>
2024-02-22 17:34:00 +0100

    Add ExcludedPrefixes option to VersioningConfig

--- minio/versioningconfig.py
+++ minio/versioningconfig.py
@@ -34,16 +34,20 @@ class VersioningConfig:
     """Versioning configuration."""
 
     def __init__(
-            self,
-            status: str | None = None,
-            mfa_delete: str | None = None,
+        self,
+        status: str | None = None,
+        mfa_delete: str | None = None,
+        excluded_prefixes: list[str] | None = None,
     ):
         if status is not None and status not in [ENABLED, SUSPENDED]:
             raise ValueError(f"status must be {ENABLED} or {SUSPENDED}")
         if mfa_delete is not None and mfa_delete not in [ENABLED, DISABLED]:
             raise ValueError(f"MFA delete must be {ENABLED} or {DISABLED}")
+        if excluded_prefixes is not None and not isinstance(excluded_prefixes, list):
+            raise ValueError(f"Excluded prefixes must be a list")
         self._status = status
         self._mfa_delete = mfa_delete
+        self._excluded_prefixes = excluded_prefixes
 
     @property
     def status(self) -> str:
@@ -55,12 +59,25 @@ class VersioningConfig:
         """Get MFA delete."""
         return self._mfa_delete
 
+    @property
+    def excluded_prefixes(self) -> str | None:
+        """Get MFA delete."""
+        return self._excluded_prefixes
+
     @classmethod
     def fromxml(cls: Type[A], element: ET.Element) -> A:
         """Create new object with values from XML element."""
         status = findtext(element, "Status")
         mfa_delete = findtext(element, "MFADelete")
-        return cls(status, mfa_delete)
+
+        excluded_prefixes_tag = element.find("ExcludedPrefixes")
+        excluded_prefixes = None
+        if excluded_prefixes_tag:
+            excluded_prefixes = [
+                tag.text for tag in excluded_prefixes_tag.findall("ExcludedPrefix")
+            ]
+
+        return cls(status, mfa_delete, excluded_prefixes)
 
     def toxml(self, element: ET.Element | None) -> ET.Element:
         """Convert to XML."""
@@ -69,4 +86,8 @@ class VersioningConfig:
             SubElement(element, "Status", self._status)
         if self._mfa_delete:
             SubElement(element, "MFADelete", self._mfa_delete)
+        if self._excluded_prefixes:
+            SubElement(element, "ExcludedPrefixes")
+            for prefix in self._excluded_prefixes:
+                SubElement(element, "ExcludedPrefix", prefix)
         return element
