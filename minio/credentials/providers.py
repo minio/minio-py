62ba3f4d8bf145d4708cbeb392e0c06cae8c0554
Setu Shah <setu4993@yahoo.co.in>
2024-08-20 22:35:25 -0700

    style: Ignore `pylint` for too many branches warning

--- minio/credentials/providers.py
+++ minio/credentials/providers.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=too-many-branches
+
 """Credential providers."""
 
 from __future__ import annotations
